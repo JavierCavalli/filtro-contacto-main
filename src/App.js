ee37321ab062e2976b3746b6e63c49a8847a7ed0
Javier Cavalli <javier.cavalli@gmail.com>
2023-01-23 16:01:08 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Table from "react-bootstrap/Table";
+import Container from "react-bootstrap/Container";
+import Form from "react-bootstrap/Form";
+import InputGroup from "react-bootstrap/InputGroup";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { data } from "./data.js";
 
 function App() {
+  const [contacts, setContacts] = useState(data);
+  const [search, setSearch] = useState("");
+
+  // const sortName = () => {
+  //   setContacts(
+  //     data.sort((a, b) => {
+  //       return a.first_name.toLowerCase() < a.first_name.toLowerCase()
+  //         ? -1
+  //         : a.first_name.toLowerCase() > a.first_name.toLowerCase()
+  //         ? 1
+  //         : 0;
+  //     })
+  //   );
+  // };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Container>
+        <h1 className="text-center mt-4">Filtro de Contactos</h1>
+        <Form>
+          <InputGroup className="my-3">
+            {/* onChange for search */}
+            <Form.Control
+              onChange={(e) => setSearch(e.target.value)}
+              placeholder="Buscar Contacto"
+            />
+          </InputGroup>
+        </Form>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Apellido</th>
+              <th>Email</th>
+              <th>Telefono</th>
+            </tr>
+          </thead>
+          <tbody>
+            {data
+              .filter((item) => {
+                return search.toLowerCase() === ""
+                  ? item
+                  : item.first_name.toLowerCase().includes(search);
+              })
+              .map((item, index) => (
+                <tr key={index}>
+                  <td>{item.first_name}</td>
+                  <td>{item.last_name}</td>
+                  <td>{item.email}</td>
+                  <td>{item.phone}</td>
+                </tr>
+              ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
